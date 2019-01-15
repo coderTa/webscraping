d3750c23e6cc833326d88c6c88c091c0a3069a91
coderTa <43300527+coderTa@users.noreply.github.com>
2019-01-14 16:15:58 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
-import requests as r
+# imports all the needed libraries
 from bs4 import BeautifulSoup as bs
+import requests as r
+from datetime import datetime
+from pymongo import MongoClient
+import time
 
-query = input("What do you want to look up? ")
-query = query.replace(' ', '+')
-result = r.get('https://www.google.com/search?q=' + query)
-text = bs(result.text)
-search_results = text.find_all("div", {"class": "g"})
-clean_search_results = []
+c = MongoClient("mongodb://techster_ben:Iltcvm50!@techsterben-cluster-shard-00-00-zx79j.gcp.mongodb.net:27017,techsterben-cluster-shard-00-01-zx79j.gcp.mongodb.net:27017,techsterben-cluster-shard-00-02-zx79j.gcp.mongodb.net:27017/test?ssl=true&replicaSet=TechsterBen-Cluster-shard-0&authSource=admin&retryWrites=true")
+database = c.Stocks
+collection = database.Popular_stocks
 
-for i in range(len(search_results)):
-    title = search_results[i].find("h3", {"class": "r"})
+cursor = collection.find({})
+for document in cursor:
+    print(document)
 
-    if title != None:
-        title = title.text
-        print(title)
+# This section of code manually adds a document to MongoDB
+# collection.insert_one({'points': 34, 'rebounds': 13, 'min': 38, 'date': datetime(2018, 12, 17)})
 
-    description = search_results[i].find("span", {"class": "st"})
+# This gets the URL & values of the stocks
+base_URL = "https://money.cnn.com/data/markets/"
 
-    if description != None:
-        description = description.text
-        print(description)
-        print("\n")
+while True:
+    page = r.get(base_URL).text
+    page = bs(page)
+    companies = page.find_all("span", {"class": "stock-name"})
+    prices = page.find_all("span", {"class": "stock-price"})
+
+    stock_prices = {}
+
+    for i in range(len(companies)):
+        companies[i] = companies[i].text
+        prices[i] = prices[i].text
+
+        stock_prices[companies[i]] = prices[i]
+
+    collection.insert_one({"stock_prices": stock_prices, "date": time.time()})
+    time.sleep(3600)
